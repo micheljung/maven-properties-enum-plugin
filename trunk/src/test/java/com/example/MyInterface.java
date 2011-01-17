69719a6064cfe32abad6ce270ec7b10fbc0d97c6
michel.jung89 <michel.jung89@61c835b3-a6b6-a8df-c1d1-a43f0ab2fe97>
2011-01-17 11:18:37 +0000

    * Changed group ID from net.sf.maven-properties-enum-plugin to com.google.code.maven-properties-enum-plugin * Renamed packages according to groupId * Have to delete old files/packages in next commit, as SVN doesn't like package renaming

--- trunk/src/test/java/com/example/MyInterface.java
+++ trunk/src/test/java/com/example/MyInterface.java
@@ -2,8 +2,6 @@ package com.example;
 
 /**
  * Interface to be implemented by generated files.
- *
- * @author <a href="mailto:michel.jung@hotmail.com">Michel Jung</a>
  */
 public interface MyInterface {
 
